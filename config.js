719e7f8f821c1625e91d68fef295b18e05ca5bf5
CHALAH374 <sethustatus416@gmail.com>
2025-08-19 19:42:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/C97VTxOwrJt3YvPLGM7aVj?mode=ac_t',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://files.catbox.moe/letrek.jpg',
+    NEWSLETTER_JID: '120363419192353625@newsletter',
+    NEWSLETTER_MESSAGE_ID: '428',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: 'CHALAH-MINI',
+    OWNER_NAME: 'CHALAH',
+    OWNER_NUMBER: '94721033354',
+    BOT_VERSION: '1.0.0',
+    BOT_FOOTER: '> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙲𝙷𝙰𝙻𝙰𝙽𝙰 𝙸𝙽𝙳𝚄𝚆𝙰𝚁𝙰',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb5OiseHltY10IBkF112/1419'
+};
